36a59da82788551007d17e52702431808fb70109
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2015-05-19 11:50:19 +0300

    Add missing files.

---
+++ themes/finna/js/finna-feed.js
@@ -0,0 +1,98 @@
+finna.feed = (function() {
+    var calculateScrollSpeed = function(scrollCnt) {
+        return 750 * Math.max(1, (scrollCnt/5));
+    };
+
+    var adjustWidth = function(holder) {
+        holder.find(".carouselSlideHeader p, .carouselText")
+            .width(holder.find(".slick-slide").width()-20);
+    };
+
+    var loadFeed = function(holder) {        
+        var id = holder.data('feed');
+        if (typeof(id) == "undefined") {
+            return;
+        }
+
+        var url = path + '/AJAX/JSON?method=getFeed&id=' + id;
+        $.getJSON(url, function(response) {
+            if (response.status === 'OK' && response.data) {
+                holder.html(response.data.html);
+                var settings = response.data.settings;
+                if (settings.type == "carousel") {
+                    console.log("set: %o", settings);
+                    holder.find(".carousel-feed").slick({
+                        dots: settings['dots'],
+                        swipe: false,
+                        infinite: true,
+                        autoplay: settings['autoplay'],
+                        autoplaySpeed: settings['autoplay'],
+                        slidesToShow: settings['slidesToShow']['desktop'],
+                        slidesToScroll: settings['scrolledItems']['desktop'],
+                        speed: calculateScrollSpeed(settings['scrolledItems']['desktop']),
+                        vertical: settings['vertical'],
+                        responsive: [
+                            {
+                                breakpoint: 992,
+                                settings: {
+                                    slidesToShow: settings['slidesToShow']['desktop-small'],
+                                    slidesToScroll: settings['scrolledItems']['desktop-small'],
+                                    speed: calculateScrollSpeed(settings['scrolledItems']['desktop-small']),                                
+                                    infinite: true,
+                                    dots: true
+                                }
+                            },
+                            {
+                                breakpoint: 650,
+                                settings: {
+                                    slidesToShow: settings['slidesToShow']['tablet'],
+                                    slidesToScroll: settings['scrolledItems']['tablet'],
+                                    speed: calculateScrollSpeed(settings['scrolledItems']['tablet']),                                
+                                    infinite: true,
+                                    dots: true
+                                }
+                            },
+                            {
+                                breakpoint: 530,
+                                settings: {
+                                    lazyLoad: 'progressive',
+                                    slidesToShow: settings['slidesToShow']['mobile'],
+                                    slidesToScroll: settings['scrolledItems']['mobile'],
+                                    speed: calculateScrollSpeed(settings['scrolledItems']['mobile']),
+                                    infinite: true,
+                                    dots: true
+                                }
+                            }
+                        ]
+                    });
+                    
+                    adjustWidth(holder);
+                    $(window).resize(function() {
+                        setTimeout(function() { adjustWidth(holder); }, 250);
+                    });
+
+                    if (typeof(settings['height']) != 'undefined') {
+                        holder.find(".slick-slide").css("max-height", settings['height'] + "px");
+                    }
+                } else {
+                    
+                }
+            }
+        });        
+    };
+    
+    var initComponents = function() {
+        $(".feed-container").each(function() {
+            loadFeed($(this));
+        });
+    };
+
+    var my = {
+        init: function() {
+            initComponents();
+        }
+    };
+
+    return my;
+
+})(finna);
