88e97cc2dbefb458458bad2d60d209c8e1ec5357
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2016-09-05 08:55:56 +0300

    Use Finna map-tiles.

--- themes/finna/js/finna-organisation-map-leaflet.js
+++ themes/finna/js/finna-organisation-map-leaflet.js
@@ -32,15 +32,14 @@ finna = $.extend(finna, {
 
             map = L.map($(holder).attr('id'), {
                 minZoom: zoomLevel.far,
-                maxZoom: 18,
+                maxZoom: 20,
                 zoomDelta: 0.1,
                 zoomSnap: 0.1
             });
 
             L.tileLayer(mapTileUrl, {
                 attribution: attribution,
-                tileSize: 512,
-                zoomOffset: -1
+                tileSize: 256
             }).addTo(map);
             
             // Center popup
