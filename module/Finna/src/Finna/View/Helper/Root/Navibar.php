a649e41f0416460c56d42a63ff9f8c4ea7478e4c
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-01-31 10:21:11 +0200

    Rafactor.

--- module/Finna/src/Finna/View/Helper/Root/Navibar.php
+++ module/Finna/src/Finna/View/Helper/Root/Navibar.php
@@ -237,12 +237,7 @@ class Navibar extends \Zend\View\Helper\AbstractHelper
 
             $data['route'] = true;
             if (strncmp($url, '/', 1) === 0) {
-                $url = vsprintf(
-                    '%s%s/%s',
-                    [$this->view->serverUrl(),
-                     $this->router->getBaseUrl(),
-                     substr($url, 1)]
-                );
+                $url = $this->view->serverUrl() . $this->router->getBaseUrl() . $url;
                 $request = new Request();
                 $request->setUri($url);
                 $routeMatch = $this->router->match($request);
