dec4eb046ffe82b90ee9c9e58b5328d0c6ab372c
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-02-05 14:32:31 +0200

    Set default status to 'open'.

--- module/Finna/sql/mysql.sql
+++ module/Finna/sql/mysql.sql
@@ -156,7 +156,7 @@ CREATE TABLE `finna_feedback` (
   `message_json` json DEFAULT '',
   `message` longtext DEFAULT '',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `status` varchar(255) DEFAULT '',
+  `status` varchar(255) DEFAULT 'open',
   `modifier_id` int(11) DEFAULT NULL,
   `modification_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
