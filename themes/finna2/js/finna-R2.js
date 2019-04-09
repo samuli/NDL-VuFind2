f9f98dbb32768c8bb6348311ad92865c3d3f2f7c
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-04-09 14:24:46 +0300

    eslint fixes.

--- themes/finna2/js/finna-R2.js
+++ themes/finna2/js/finna-R2.js
@@ -1,10 +1,10 @@
-/*global finna, jQuery */
+/*global finna, VuFind */
 finna.R2 = (function finnaR2() {
 
   function initModal() {
     // Transform form h1-element to a h2 so that the modal gets a proper title bar
     var modal = $('#modal');
-    modal.on('show.bs.modal', function (e) {
+    modal.on('show.bs.modal', function onShowModal (/*e*/) {
       var title = $(this).find('.feedback-content h1:first-child');
       if (title.length > 0) {
         var body = $(this).find('.modal-body');
@@ -20,8 +20,7 @@ finna.R2 = (function finnaR2() {
   }
   
   function initCheckPermission() {
-    $('div.check-permission').not('.inited').each(function initCheckPermission() {
-      var el = $(this);
+    $('div.check-permission').not('.inited').each(function addCheckPermission() {
       var id = $(this).data('id');
 
       var url = VuFind.path + '/AJAX/JSON?method=getRemsPermission';
