87fbe3178c1f0175df12af1a132f12c13bd775b3
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-04-16 11:52:41 +0300

    [ALLI-5807] Suomi.fi authentication.

--- module/Finna/src/Finna/Auth/Shibboleth.php
+++ module/Finna/src/Finna/Auth/Shibboleth.php
@@ -46,6 +46,57 @@ use VuFind\Exception\Auth as AuthException;
 class Shibboleth extends \VuFind\Auth\Shibboleth
 {
     /**
+     * IDP (null for default Shibboleth handler HAKA)
+     * This determines configuration and templates.
+     *
+     * @var string|null
+     */
+    protected $idp = null;
+
+    /**
+     * Set IDP
+     *
+     * @param string $idp IDP
+     *
+     * @return void
+     */
+    public function setIdp($idp)
+    {
+        $this->idp = $idp;
+    }
+
+    /**
+     * Get IDP
+     *
+     * @return null|string
+     */
+    public function getIdp()
+    {
+        return $this->idp;
+    }
+
+    /**
+     * Get configuration (load automatically if not previously set).  Throw an
+     * exception if the configuration is invalid.
+     *
+     * @throws AuthException
+     * @return \Zend\Config\Config
+     */
+    public function getConfig()
+    {
+        if ($this->idp && !$this->configValidated) {
+            // Replace Shibboleth config section with
+            // IDP specific section
+            $section = 'Shibboleth_' . $this->idp;
+            $data = $this->config->toArray();
+            $data['Shibboleth'] = $data[$section];
+            $this->config = new \Zend\Config\Config($data);
+        }
+
+        return parent::getConfig();
+    }
+
+    /**
      * Attempt to authenticate the current user.  Throws exception if login fails.
      *
      * @param \Zend\Http\PhpEnvironment\Request $request Request object containing
@@ -178,8 +229,17 @@ class Shibboleth extends \VuFind\Auth\Shibboleth
      */
     protected function getServerParam($request, $param)
     {
-        return $request->getServer()->get(
+        $val = $request->getServer()->get(
             $param, $request->getServer()->get("REDIRECT_$param")
         );
+
+        $config = $this->getConfig()->Shibboleth;
+        if ($parm === $config->username
+            && ((bool)$config->hash_username ?? false)
+            && $secret = ($config->hash_secret ?? null)
+        ) {
+            $val = hash_hmac('sha256', $val, $secret, true);
+        }
+        return $val;
     }
 }
