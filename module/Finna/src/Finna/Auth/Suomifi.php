e8ec5a9fecbd5f9eb4758f82100cec92b86db98d
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-04-16 13:59:33 +0300

    Suomifi authentication.

---
+++ module/Finna/src/Finna/Auth/Suomifi.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Suomi.fi authentication module.
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Villanova University 2010.
+ * Copyright (C) The National Library of Finland 2015-2019.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category VuFind
+ * @package  Authentication
+ * @author   Franck Borel <franck.borel@gbv.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Page
+ */
+namespace Finna\Auth;
+
+/**
+ * Suomi.fi authentication module.
+ *
+ * @category VuFind
+ * @package  Authentication
+ * @author   Franck Borel <franck.borel@gbv.de>
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @author   Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org Main Page
+ */
+class Suomifi extends Shibboleth
+{
+    /**
+     * Get configuration (load automatically if not previously set).  Throw an
+     * exception if the configuration is invalid.
+     *
+     * @throws AuthException
+     * @return \Zend\Config\Config
+     */
+    public function getConfig()
+    {
+        if (!$this->configValidated) {
+            // Replace Shibboleth config section with Shibboleth_suomifi
+            $data = $this->config->toArray();
+            $data['Shibboleth'] = $data['Shibboleth_suomifi'];
+            $this->config = new \Zend\Config\Config($data);
+        }
+
+        return parent::getConfig();
+    }
+}
