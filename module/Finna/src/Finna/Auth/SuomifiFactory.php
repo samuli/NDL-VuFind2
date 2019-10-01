0031ffae4f84c80973580bade6483630517ea678
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-10-01 13:54:54 +0300

    Fix typo.

--- module/Finna/src/Finna/Auth/SuomifiFactory.php
+++ module/Finna/src/Finna/Auth/SuomifiFactory.php
@@ -38,7 +38,7 @@ use Interop\Container\ContainerInterface;
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
-class SuomifiFactory extends \VuFind\Auth\ShibolethFactory
+class SuomifiFactory extends \VuFind\Auth\ShibbolethFactory
 {
     /**
      * Create an object
