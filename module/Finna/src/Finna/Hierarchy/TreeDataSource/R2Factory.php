98cf6b665a427a7372f8c43f8c9ef618555361d0
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-10-28 14:51:00 +0200

    Simplify.

--- module/Finna/src/Finna/Hierarchy/TreeDataSource/R2Factory.php
+++ module/Finna/src/Finna/Hierarchy/TreeDataSource/R2Factory.php
@@ -46,24 +46,4 @@ class R2Factory extends \VuFind\Hierarchy\TreeDataSource\SolrFactory
      * @var string
      */
     protected $backendId = 'R2';
-
-    /**
-     * Create an object
-     *
-     * @param ContainerInterface $container     Service manager
-     * @param string             $requestedName Service being created
-     * @param null|array         $options       Extra options (optional)
-     *
-     * @return object
-     *
-     * @throws ServiceNotFoundException if unable to resolve the service.
-     * @throws ServiceNotCreatedException if an exception is raised when
-     * creating a service.
-     * @throws ContainerException if any other error occurs
-     */
-    public function __invoke(ContainerInterface $container, $requestedName,
-        array $options = null
-    ) {
-        return parent::__invoke(...func_get_args());
-    }
 }
