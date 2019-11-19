dcacf392aaa1aa1b96af73f3c5c7aa05f81e1fc8
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2019-11-19 16:13:51 +0200

    Convert author-id facet label to a readable form when listing facets.

--- module/Finna/src/Finna/Controller/SearchController.php
+++ module/Finna/src/Finna/Controller/SearchController.php
@@ -285,6 +285,30 @@ class SearchController extends \VuFind\Controller\SearchController
     }
 
     /**
+     * Returns a list of all items associated with one facet for the lightbox
+     *
+     * Parameters:
+     * facet        The facet to retrieve
+     * searchParams Facet search params from $results->getUrlQuery()->getParams()
+     *
+     * @return mixed
+     */
+    public function facetListAction()
+    {
+        $authorityHelper
+            = $this->serviceLocator->get(\Finna\Search\Solr\AuthorityHelper::class);
+
+        $view = parent::facetListAction();
+
+        // Convert author-id facet labels to readable names
+        $view->data = $authorityHelper->formatFacets(
+            \Finna\Search\Solr\AuthorityHelper::AUTHOR2_ID_FACET,
+            $view->data
+        );
+        return $view;
+    }
+
+    /**
      * Handler for database and journal browse actions.
      *
      * @param string $type Browse type
