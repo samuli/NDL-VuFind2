5b06c9868e39b3604e3b03ef93e5256ba02393aa
Juha Luoma <juhaluoma5@gmail.com>
2020-05-29 16:42:39 +0300

    Clean js file

--- themes/finna2/js/finna-dynamic-list.js
+++ themes/finna2/js/finna-dynamic-list.js
@@ -1,91 +1,92 @@
 /*global VuFind, finna */
 finna.dynamicList = (function finnaDynamicList() {
-    var settings = {
-        dots: true,
-        swipe: true,
-        lazyload: 'ondemand',
-        responsive: [
-            {
-                breakpoint: 5000,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5
-                }
-            },
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3
-                }
-            },
-            {
-                breakpoint: 500,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }
-          ]
-    }
-    function initSlick() {
-        $('.dynamic-list-wrapper').each(function initDynamicList() {
-            $(this).one('inview', function getList() {
-                var _ = $(this);
-                url = _.data('url');
-                if (url.length) {
-                    $.getJSON(VuFind.path + url).done(function parseResult(response) {
-                        _.append(response.data.html);
-                        _.find('.dynamic-list-item').each(function adjustImages() {
-                            var img = $(this).find('img');
-                            if (img.length) {
-                                img.on('load', function checkImage() {
-                                    handleImage($(this));
-                                });
-                            } else {
-                                $(this).find('.hidden').removeClass('hidden');
-                                $(this).find('.dynamic-list-title').addClass('no-image');
-                            }
-                        });                
-                        _.find('.content').slick(settings);
-                    }).fail(function onFailure(response) {
-                        _.append(response);
-                        console.log(response);
-                    });
-                }
-            });
-        });
-    }
+  var settings = {
+    dots: true,
+    swipe: true,
+    lazyload: 'ondemand',
+    responsive: [
+      {
+        breakpoint: 5000,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  };
 
-    function initSearch() {
-        $('.dynamic-list-result').each(function adjustImages() {
-            var img = $(this).find('img');
-            if (img.length) {
-                img.unveil(100, function handleIcon() {
-                    $(this).on('load', function onImageLoaded() {
-                        handleImage($(this));
-                    });
+  function handleImage(img) {
+    var i = img[0];
+    if (i.naturalWidth && i.naturalWidth === 10 && i.naturalHeight === 10) {
+      img.hide();
+      img.siblings('.hidden').removeClass('hidden');
+      img.siblings('.dynamic-list-title').addClass('no-image');
+    }
+  }
+
+  function initSlick() {
+    $('.dynamic-list-wrapper').each(function initDynamicList() {
+      $(this).one('inview', function getList() {
+        var _ = $(this);
+        var url = _.data('url');
+        if (url.length) {
+          $.getJSON(VuFind.path + url).done(function parseResult(response) {
+            _.append(response.data.html);
+            _.find('.dynamic-list-item').each(function adjustImages() {
+              var img = $(this).find('img');
+              if (img.length) {
+                img.on('load', function checkImage() {
+                  handleImage($(this));
                 });
-            } else {
+              } else {
                 $(this).find('.hidden').removeClass('hidden');
-            }
+                $(this).find('.dynamic-list-title').addClass('no-image');
+              }
+            });                
+            _.find('.content').slick(settings);
+          }).fail(function onFailure() {
+                      
+          });
+        }
+      });
+    });
+  }
+
+  function initSearch() {
+    $('.dynamic-list-result').each(function adjustImages() {
+      var img = $(this).find('img');
+      if (img.length) {
+        img.unveil(100, function handleIcon() {
+          $(this).on('load', function onImageLoaded() {
+            handleImage($(this));
+          });
         });
-    }
+      } else {
+        $(this).find('.hidden').removeClass('hidden');
+      }
+    });
+  }
 
-    function handleImage(img) {
-        var i = img[0];
-        if (i.naturalWidth && i.naturalWidth == 10 && i.naturalHeight == 10) {
-            img.hide();
-            img.siblings('.hidden').removeClass('hidden');
-            img.siblings('.dynamic-list-title').addClass('no-image');
-        }
+  var my = {
+    init: function init() {
+      initSlick();
+      initSearch();
     }
+  };
 
-    var my = {
-        init: function init() {
-            initSlick();
-            initSearch();
-        }
-    };
-    return my;
+  return my;
 })();
