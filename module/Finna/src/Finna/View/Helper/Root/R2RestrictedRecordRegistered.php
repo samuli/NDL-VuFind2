eaf9ea617287f95b8178d5444e724646a5c1e8a5
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2020-10-05 14:27:07 +0300

    Fix typo.

--- module/Finna/src/Finna/View/Helper/Root/R2RestrictedRecordRegistered.php
+++ module/Finna/src/Finna/View/Helper/Root/R2RestrictedRecordRegistered.php
@@ -105,7 +105,7 @@ class R2RestrictedRecordRegistered extends \Laminas\View\Helper\AbstractHelper
             }
 
             $warning = null;
-            if ($this->rems->isSearchLimitExceeded('monthly')) {
+            if ($this->rems->isSearchLimitExceeded('daily')) {
                 $warning = 'R2_daily_limit_exceeded';
             } else if ($this->rems->isSearchLimitExceeded('monthly')) {
                 $warning = 'R2_monthly_limit_exceeded';
